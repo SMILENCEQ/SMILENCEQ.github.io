35708418ba9e02d3ba5a29ec0e16f6e77a5b966c
SMILENCEQ <hpneed977@outlook.com>
2024-11-21 23:42:32 +0800

    4.11.21

--- csh.sh
+++ csh.sh
@@ -68,26 +68,14 @@ color () {
 
 
 
-#███████╗██████╗ ██╗   ██╗███╗   ██╗
-#██╔════╝██╔══██╗██║   ██║████╗  ██║
-#███████╗██████╔╝██║   ██║██╔██╗ ██║
-#╚════██║██╔══██╗██║   ██║██║╚██╗██║
-#███████║██║  ██║╚██████╔╝██║ ╚████║
-#╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
-#
-#  ███████╔ ████████═ ██╗   ██╗ 
-# ██══════╝ ██ ╔══════██║   ██║ 
-#██         ████████═ ████████║ 
-#╚██        ╚════ ██  ██║   ██║ 
-#  ███████╔ ████████  ██║   ██║ 
-#  ╚══════╝ ╚════════╝╚═╝   ╚═╝                                                                    
+                                                             
 #
 
 
 
 ##########################################
 ## Starting
-CshVersion=v4.11.13
+CshVersion=v4.11.21
 echo  "============================================================"
 echo -e "\e[1;$[RANDOM%7+31]m
 
@@ -208,6 +196,12 @@ initialize_variables() {
             PerlVersion=5.36.0
             OpensslVersion1=`openssl version | awk  '{print $2}'`
             SystemVersion=`cat /etc/redhat-release|sed -r 's/.* ([0-9]+)\..*/\1/'`
+            ;;
+        update_sshssl6)
+            OpensslVersion=3.3.1
+            OpensslVersion1=`openssl version | awk  '{print $2}'`
+            OpensshVersion=9.8p1
+            Date=`date +%Y%m%d%H%M%S`
             ;;
         *)
             echo "Unknown function: $1"
@@ -4764,6 +4758,9 @@ else
 fi
 
 
+
+
+
 echo -e "\e[1;35m==========================安装依赖包================================\e[0m"
 
 
@@ -4855,13 +4852,6 @@ fi
 
 
 
-echo "执行网络检测"
-if ping -c 1 www.baidu.com >/dev/null;then
-    echo -e "\e[1;32m"网络正常"\e[0m"
-else
-    echo -e "\e[1;31m"网络不通"\e[0m"
-    
-fi
 
 
 
@@ -5323,7 +5313,189 @@ ssh -V
 
 
 
+#===================================================================================================================================================================
+#适用于openEuler系统升级openssh9.8p1和openssl3.3.1
 
+update_sshssl6(){
+initialize_variables "update_sshssl6"
+echo -e "\e[1;35m====================================================================\e[0m"
+echo -e "\e[1;35m现在已安装的版本\e[0m"
+ssh -V
+echo -e "\e[1;35m本次安装的版本是openssl-${OpensslVersion}\e[0m"
+echo -e "\e[1;35m本次升级的安装版本openssh-${OpensshVersion}\e[0m"
+echo -e "\e[1;35m离线安装，请提前准备好对应版本的压缩包放在root目录下\e[0m"
+echo -e "\e[1;35m====================================================================\e[0m"
+echo -e "\e[1;35m不想安装请在五秒内终止脚本\e[0m\n"
+
+## 关闭严格模式
+set +u
+source /etc/profile.d/lang.sh
+
+for i in {5..1}
+do
+    echo -n "${i} "
+    echo -ne "\r"
+    sleep 1
+done
+
+
+echo -e "\e[1;35m==========================安装telnet=================================\e[0m"
+
+software=(
+    "telnet"
+    "telnet-server"
+    "xinetd"
+    )
+for i in ${software[@]}
+do
+rpm -q $i &> /dev/null && echo -e "$i\t\e[1;32m已安装\e[0m" || { yum -y install $i &> /dev/null; echo -e "$i\t\e[1;35m安装成功\e[0m" ; }
+done
+
+
+systemctl enable --now  xinetd.service
+systemctl enable --now  telnet.socket
+
+
+cat >>/etc/securetty<<EOF
+pts/0 
+pts/1
+EOF
+
+
+
+echo -e "\e[1;35m手动测试telnet连接是否正常\e[0m"
+
+echo -e "\e[1;35m==========================检查系统==================================\e[0m"
+
+if [ $ID = 'openEuler' ];then
+    echo -e "\e[1;35m检测系统为openEuler,脚本支持执行\e[0m"
+else
+    echo -e "\e[1;33m检测系统不是openEuler,脚本不支持\e[0m"
+    exit
+fi
+
+
+if [[ ${OpensslVersion1}  < 3 ]];then
+    echo -e "\e[1;35m脚本支持升级openssl\e[0m"
+else
+    echo -e "\e[1;33m检测openssl版本高于3版本,脚本不支持\e[0m"
+    exit
+fi
+
+
+
+
+
+echo -e "\e[1;35m==========================源文件检查================================\e[0m"
+if [ -e "openssl-${OpensslVersion}.tar.gz" ];then
+    echo -e "\e[1;35mopenssl-${OpensslVersion}文件存在\e[0m"
+else
+    echo -e "\e[1;33mopenssl-${OpensslVersion}文件不存在，开始下载\e[0m"
+    exit
+fi
+
+
+
+if [[ -a "openssh-${OpensshVersion}.tar.gz" ]];then
+    echo -e "\e[1;35mopenssh-${OpensshVersion}文件存在\e[0m"
+else
+    echo -e "\e[1;33mopenssh-${OpensshVersion}文件不存在\e[0m"
+    exit
+fi
+
+
+
+echo -e "\e[1;35m=======================开始升级openssl==============================\e[0m"
+echo -e "\e[1;35m==========================安装依赖包================================\e[0m"
+
+
+#yum install  -y gcc gcc-c++ glibc make
+software=(
+    "gcc"
+    "gcc-c++"
+    "openssl-devel"
+    "zlib-devel"
+    "make"
+    "glibc"
+    "autoconf"
+    "pcre-devel"
+    "pam-devel"
+    "pam*"
+    )
+for i in ${software[@]}
+do
+rpm -q $i &> /dev/null && echo -e "$i\t\e[1;32m已安装\e[0m" || { yum -y install $i &> /dev/null; echo -e "$i\t\e[1;35m安装成功\e[0m" ; }
+done
+
+
+
+
+
+
+
+
+echo -e "\e[1;35m==========================编译安装=======================================\e[0m"
+tar -zxvf openssl-${OpensslVersion}.tar.gz
+cd openssl-${OpensslVersion}
+./config --prefix=/usr/local/openssl --openssldir=/usr/local/openssl shared zlib
+
+
+make && make install
+
+sudo mv /usr/bin/openssl /usr/bin/openssl-${Date}
+sudo ln -s /usr/local/openssl/bin/openssl /usr/bin/openssl
+sudo echo "/usr/local/openssl/lib" >> /etc/ld.so.conf
+sudo echo "/usr/local/openssl/lib64" > /etc/ld.so.conf.d/openssl.conf
+sudo ldconfig -v
+
+cd
+#查看版本 是否安装成功
+echo -e "\e[1;35m===============================升级后版本================================\e[0m"
+ldconfig
+openssl version
+
+
+
+
+sleep 5
+
+
+
+echo -e "\e[1;35m=======================开始升级openssh==================================\e[0m"
+
+echo -e "\e[1;35m===========================解压源文件====================================\e[0m"
+
+
+tar zxf /root/openssh-${OpensshVersion}.tar.gz
+if [ -e /root/openssh-${OpensshVersion} ];then
+    echo -e "\e[1;35mopenssh解压成功\e[0m"
+else
+    echo -e "\e[1;35mopenssh解压失败\e[0m"
+    exit
+fi
+
+
+
+
+echo -e "\e[1;35m===========================编译openssh==================================\e[0m"
+
+cd /root/openssh-${OpensshVersion}/
+./configure  --with-ssl-dir=/usr/local/openssl
+ make && make install
+
+systemctl restart sshd
+
+sleep 3
+echo -e "\e[1;35m=========================更新后openssh版本=================================\e[0m"
+#export LD_LIBRARY_PATH=/usr/local/openssl/lib64:$LD_LIBRARY_PATH
+echo 'export LD_LIBRARY_PATH=/usr/local/openssl/lib64:${LD_LIBRARY_PATH:-""}' >> /etc/profile
+echo 'export PATH=/usr/local/bin:$PATH' >>/etc/profile
+ldconfig
+source /etc/profile
+ssh -V
+
+
+}
 
 
 
@@ -8732,7 +8904,8 @@ update_sshssl1---适用于从openssl1.*版本升级到openssl1.1.1w,openssh9.7p1
 update_sshssl2---适用于从1.*版本升级到openssl3.3.0,openssh9.7p1
 update_sshssl3---适用于从3.*版本升级到openssl3.3.1,openssh9.8p1
 update_sshssl4---适用于从1.*版本升级到openssl3.3.1,openssh9.8p1
-update_sshssl5---适用与centos6系统升级openssh9.8p1和openssl3.3.1
+update_sshssl5---适用于centos6系统升级openssh9.8p1和openssl3.3.1
+update_sshssl6---适用于openeuler系统升级openssh9.8p1和openssl3.3.1
 EOF
 echo -e "\E[0m"
 
@@ -8914,6 +9087,7 @@ if [[ -n "${1-}" ]];then
         update_sshssl3 |\
         update_sshssl4 |\
         update_sshssl5 |\
+        update_sshssl6 |\
         c7_yum_docker |\
         c7_yum2_docker |\
         c8_yum_docker |\
@@ -10794,6 +10968,7 @@ while :;do
     cat << EOF
 
 ============================================================
+
 **********以下支持在centos7系列使用*************
 3.升级openssl到1.1.1w版本
 4.升级openssh到9.7p1版本
@@ -10801,8 +10976,13 @@ while :;do
 6 适用于从openssl1.*版本升级到openssl3.3.0,openssh9.7p1
 7.适用于从openssl3.*版本升级到openssl3.3.1,openssh9.8p1
 8.适用于从openssl1.*版本升级到openssl3.3.1,openssh9.8p1
+
 **********以下支持在centos6系列使用*************
 9.适用于从openssl1.*版本升级到openssl3.3.1,openssh9.8p1
+
+**********以下支持在openEuler系列使用*************
+10.适用于从openssl1.*版本升级到openssl3.3.1,openssh9.8p1
+
 ============================================================
 
 ************************************************************
@@ -10821,6 +11001,7 @@ while :;do
 **********           7.升级openssl,openssh        **********
 **********           8.升级openssl,openssh        **********
 **********           9.升级openssl,openssh        **********
+**********           10.升级openssl,openssh       **********
 **********                                        **********
 **********           0.退出                       ********** 
 **********                                        **********
@@ -10861,6 +11042,9 @@ case $Menu in
         ;;
 
 9)     update_sshssl5
+        ;;
+
+10)    update_sshssl6
         ;;
 
 0)     set_et
