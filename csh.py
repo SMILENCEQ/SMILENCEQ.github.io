9b0fcba8fb15f6a6a58c60708f70775f3af52c82
SMILENCEQ <hpneed977@outlook.com>
2025-02-27 12:02:14 +0800

    v0.0.3

--- csh.py
+++ csh.py
@@ -5,21 +5,34 @@ Linux 运维脚本,基于csh.sh脚本进行Python重构
 ''')
 
 def set_all():
-    print('构建中')
+    print('Linux系统设置构建中')
 
 def service_install():
-    print('构建中')
+    print('服务安装构建中')
 
-print("""
-Linux 运维脚本
-1.设置Linux系统配置
-2.服务安装
-""")
 
-csh_all = int(input('请输入功能菜单序号: '))
-if csh_all == 1:
-    set_all()
-elif csh_all == 2:
-    service_install()
-else:
-    print('输入错误!')
+def main():
+    while True:
+        print("""
+        Linux 运维脚本
+        1.设置Linux系统配置
+        2.服务安装
+        0.退出
+        """)
+        try:
+            csh_all = int(input('请输入功能菜单序号: '))
+        except ValueError:
+            print('输入错误! 请输入数字。')
+            continue  # 继续循环，不退出
+
+        if csh_all == 1:
+            set_all()
+        elif csh_all == 2:
+            service_install()
+        elif csh_all == 0:
+            print("退出")
+            break
+        else:
+            print('输入错误!')
+if __name__ == "__main__":
+    main()
